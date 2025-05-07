db013455e48e7b7e777cf64414eea9f952e76aec
Jamesy123Paul <paul>
2025-05-07 08:11:30 +0100

    Add h1 tag to hellow world and add a subtitle with welcome to my site on program

--- vs-2031-github-collaboration/Program.cs
+++ vs-2031-github-collaboration/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "<h1>Hello World</h1><br>!\nWelcome to my site!");
+app.MapGet("/", () => @"Hello World!
+Welcome to my site");
 
 app.Run();
